c7f6a190234fa458a166110dcdcdc0cf68c33bf9
Juan-manuel Clavijo <clavijo.j@gmail.com>
2016-12-06 15:05:27 -0800

    make-primes empty project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to make-primes
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/make_primes/core_test.clj
@@ -0,0 +1,7 @@
+(ns make-primes.core-test
+  (:require [clojure.test :refer :all]
+            [make-primes.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
